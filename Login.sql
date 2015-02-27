499c5c190654c26b90f36861c06cd8c3e47b3aa1
Josh <chang101@aol.com>
2015-02-27 11:02:04 -0500

    Added Firstname, Lastname and email in login table

--- Login.sql
+++ Login.sql
@@ -3,11 +3,17 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: gsarastestdb.gsaras.com
--- Generation Time: Feb 24, 2015 at 01:00 PM
+-- Generation Time: Feb 27, 2015 at 07:59 AM
 -- Server version: 5.1.56
 -- PHP Version: 5.4.37
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `gsarastestdb`
@@ -21,8 +27,10 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `Login` (
   `loginNum` mediumint(9) NOT NULL AUTO_INCREMENT,
-  `Username` text NOT NULL,
+  `Email` text NOT NULL,
   `Password` text NOT NULL,
+  `FirstName` text NOT NULL,
+  `LastName` text NOT NULL,
   PRIMARY KEY (`loginNum`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
