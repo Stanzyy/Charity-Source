f6a4748ce9d3649d53468b267aa508bd5dc2e6a1
mlgGreggers <gregorysaras@gmail.com>
2015-02-28 14:03:38 -0500

    Form Validation - GDC
    jQuery form validation for signin/signup. Is set up to also accept the
    server errors such as user not found or wrong password

--- signup.php
+++ signup.php
@@ -11,13 +11,19 @@
                 <?php require_once('header.php');?>
                 <!--Won't be using the proper form tags here since we are dealing with passwords. For the sake of this project, setting up secure connections with certificates is not a priority, so we will be going around built in form security which will not allow the submission of passwords over a non-secure connection. -->
                 <div id="leftForm">
-                    <h3>Sign In</h3>
+                    <div class="formHead">
+                        <h3>Sign In</h3>
+                        <span id="signinErrorContainer"><p id="signInError">Error Space</p></span>
+                    </div>
                     <div class="inputField"><input class="inputBox" id="userName" type="email" placeholder="Email"></div>
 					<div class="inputField"><input class="inputBox" id="existingPassword" type="password" placeholder="Password"></div>
                     <div class="inputField buttonHolder"><button class="submitButton" id="signInButton" type="submit">Submit</button></div>			    
                 </div><!--
-             --><div id="rightForm">                    
-                    <h3>Sign Up</h3>
+             --><div id="rightForm">
+                    <div class="formHead">
+                        <h3>Sign Up</h3>
+                        <span id="signupErrorConatiner"><p id="signupError">Error Space</p></span>
+                    </div>
                     <div class="inputField"><input class="inputBox" id="firstName" type="test" placeholder="First Name"></div>
                     <div class="inputField"><input class="inputBox" id="lastName" type="text" placeholder="Last Name"></div>
                     <div class="inputField"><input class="inputBox" id="newUser" type="email" placeholder="Email"></div>
@@ -63,10 +69,23 @@
                 },
                 
                 signin: function(){
+                    $("#signinErrorContainer").css("visibility", "hidden");
                     //sign user in
                     var userName = $("#userName").val();
                     var pword = $("#existingPassword").val();
-                    
+                    //Validte this guy
+                    if(userName === "" || userName.indexOf('@') == -1){
+                        $("#userName").focus();    
+                        $("#signInError").empty().append("Please Enter Your Email");
+                        $("#signinErrorContainer").css("visibility", "visible");
+                        return;
+                    }
+                    if(pword === ""){
+                        $("#existingPassword").focus();
+                        $("#signInError").empty().append("Please Enter Your Password");
+                        $("#signinErrorContainer").css("visibility", "visible");
+                        return;
+                    }
                     $.ajax({
 				        type: "GET",
 						url: "functions.php",
@@ -86,6 +105,7 @@
                 },
                 
                 signup: function(){
+                    $("#signupErrorConatiner").css("visibility", "hidden");
                     //sign up user
                     var userName = $("#newUser").val();
                     var firstName = $("#firstName").val();
@@ -93,25 +113,68 @@
                     var pword = $("#newPassword").val();
                     var pwordCheck = $("#newPasswordCheck").val();
                     
+                    if(firstName === ""){
+                        $("#firstName").focus();
+                        $("#signupError").empty().append("Please Enter Your First Name.");
+                        $("#signupErrorConatiner").css("visibility", "visible");
+                        return;
+                    }
+                    
+                    if(lastName === ""){
+                        $("#lastName").focus();
+                        $("#signupError").empty().append("Please Enter Your Last Name.");
+                        $("#signupErrorConatiner").css("visibility", "visible");
+                        return;
+                    }
+                    
+                    if(userName === "" || userName.indexOf('@') == -1){
+                        $("#newUser").focus();
+                        $("#signupError").empty().append("Please Enter a valid Email.");
+                        $("#signupErrorConatiner").css("visibility", "visible");
+                        return;
+                    }
+                    
+                    if(pword === ""){
+                        $("#newPassword").focus();
+                        $("#signupError").empty().append("Please Enter a Password.");
+                        $("#signupErrorConatiner").css("visibility", "visible");
+                        return;
+                    }
+                    
+                    if(pwordCheck === ""){
+                        $("#newPasswordCheck").focus();
+                        $("#signupError").empty().append("Please re-enter your Password.");
+                        $("#signupErrorConatiner").css("visibility", "visible");
+                        return;
+                    }
+                    
+                    if(pword !== pwordCheck){
+                        $("#newPassword").focus();
+                        $("#signupError").empty().append("Passwords do not match up.");
+                        $("#signupErrorConatiner").css("visibility", "visible");
+                        return;
+                    }
+                    
                     $.ajax({
-				        type: "GET",
-						url: "functions.php",
-						data: {
-                            action: "signUserUp",
-						    userName: userName,
-                            firstName: firstName,
-                            lastName: lastName,
-						    pword: pword,
-				            pwordCheck: pwordCheck
-				        },
-						success: function(returnedData){
-                            alert(returnedData);
-                            $("#popup")[0].click();
-						},
-						error: function(returnedData){
-						  alert("There was an error with your request, please try again.");
-						}
-				    });
+				            type: "GET",
+				            url: "functions.php",
+						    data: {
+                                action: "signUserUp",
+						        userName: userName,
+                                firstName: firstName,
+                                lastName: lastName,
+						        pword: pword,
+				                pwordCheck: pwordCheck
+				            },
+						    success: function(returnedData){
+                                alert(returnedData);
+                                $("#popup")[0].click();
+						    },
+						    error: function(returnedData){
+						        alert("There was an error with your request, please try again.");
+						    }
+				        });
+                    
                 },
                 
                 signOut: function(){
