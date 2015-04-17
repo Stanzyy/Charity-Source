a52041d7df57b12d94bda812d2e1f5f91a3a02a9
mlgGreggers <gregorysaras@gmail.com>
2015-04-17 07:50:02 -0400

    Added a meet the team page
    ^^

--- about.php
+++ about.php
@@ -6,52 +6,86 @@
   <!--      <link rel="stylesheet" type="text/css" href="css/nick.css">
 		<link rel="stylesheet" type="text/css" href="css/gsarascss.css">
 -->
-        <title>About | Charity Source</title>
+        <title>The Team | Charity Source</title>
         <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
         <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+        <style type="text/css">
+			 .sponsorCenter{
+				width: 945px;
+				margin: 0px auto;
+				text-align: center;
+				height: 20em;
+				background-color: #fefefe;
+			}
+
+			.sponsorItem{
+				margin-top: 1em;
+				margin-bottom: 3em;
+				clear: both;
+				border-bottom: 1px solid #231F20;
+				padding-bottom: 1em;
+			}
+
+			.sponsorImg{
+				height: 150px;
+				margin-top: 4.8em;
+				display: inline-block;
+			}
+
+			.sponsorText{
+				width: 70%;
+				text-align: left;
+				display: inline-block;
+				vertical-align: top;
+				margin-top: 5.6em;
+				margin-left: 2em;	
+				letter-spacing: 1px;
+				line-height: 1.5em;
+			}
+
+			.title{
+				float: left;
+				margin-bottom: -2em;
+			}
+
+			.lastItem{
+				border-bottom: none;
+			}
+        </style>
 	</head>
 	<body>
 		<div class="wrapper container">
-			<div class="aboutContent"> <!-- aboutContent div containing all the content on the about page -->
-				<?php require_once('header.php');?>
-                <h3>About Us</h3>
-				<div class="aboutDescription">
-                    <!-- description about charity source as a whole -->
-                    <p>Charity Source will provide a fast, efficient way for users to donate to and support their 
-                        favorite charities throughout the United States. The main goal is to remove the ambiguity of what
-                        charities do with the donations they receive. It will also be an easy way to donate to a good cause
-                        that will leave people feeling comfortable knowing where their contributions are going. 
-                    </p>
-                </div>
-				<h3>What We're About</h3>
-				<div class="aboutDescription">
-                    <!-- description about charity source team -->
-                    <p>Our team at Charity Source is dedicated to providing a safe and user friendly environment in which
-                        people can donate to good causes and feel great about it!                 
-                    </p>
-                </div>
-				<h3>Meet the Team</h3>
-                <div class="teamMembers row">
-                <!-- images of each team member -->
-				<div class="person col-sm-3 col-md-3">
-					<img src="images/greg.png" />
-					<p>Gregory Saras</p>
+			<?php require_once('header.php');?>
+			<div class="sponsorCenter">
+					<div class="sponsorItem">
+						<h2 class="title">Nicolas Stanzione</h2>
+						<img src="images/nick.png" class="sponsorImg" />
+						<p class="sponsorText">Nouria Energy Corporation is a fuel distributor supplying gasoline and diesel to over 125 service stations. We operate 83 convenience stores with fuel; 
+						some locations also consist of quick serve restaurants such as Dunkin Donuts, McDonald’s and Subway. We also take pride in our Lil Mart brand locations in 
+						Maine. Our corporate headquarters are situated in Worcester, Massachusetts with an international support center in Lebanon.</p>
+					</div>
+					<div class="sponsorItem">
+						<h2 class="title">Josh Blanchette</h2>
+						<img src="images/josh.png" class="sponsorImg" />
+						<p class="sponsorText">Nouria Energy Corporation is a fuel distributor supplying gasoline and diesel to over 125 service stations. We operate 83 convenience stores with fuel; 
+						some locations also consist of quick serve restaurants such as Dunkin Donuts, McDonald’s and Subway. We also take pride in our Lil Mart brand locations in 
+						Maine. Our corporate headquarters are situated in Worcester, Massachusetts with an international support center in Lebanon.</p>
+					</div>
+					<div class="sponsorItem">
+						<h2 class="title">Eric Getchell</h2>
+						<img src="images/eric.jpg" class="sponsorImg" />
+						<p class="sponsorText">Nouria Energy Corporation is a fuel distributor supplying gasoline and diesel to over 125 service stations. We operate 83 convenience stores with fuel; 
+						some locations also consist of quick serve restaurants such as Dunkin Donuts, McDonald’s and Subway. We also take pride in our Lil Mart brand locations in 
+						Maine. Our corporate headquarters are situated in Worcester, Massachusetts with an international support center in Lebanon.</p>
+					</div>
+					<div class="sponsorItem lastItem">
+						<h2 class="title">Gregory Saras</h2>
+						<img src="images/greg.png" class="sponsorImg" />
+						<p class="sponsorText">Nouria Energy Corporation is a fuel distributor supplying gasoline and diesel to over 125 service stations. We operate 83 convenience stores with fuel; 
+						some locations also consist of quick serve restaurants such as Dunkin Donuts, McDonald’s and Subway. We also take pride in our Lil Mart brand locations in 
+						Maine. Our corporate headquarters are situated in Worcester, Massachusetts with an international support center in Lebanon.</p>
+					</div>
 				</div>
-				<div class="person col-sm-3 col-md-3">
-					<img src="images/eric.jpg" />
-					<p>Eric Getchell</p>
-				</div>
-				<div class="person col-sm-3 col-md-3">
-					<img src="images/josh.png" />
-					<p>Josh Blanchette</p>
-				</div>
-				<div class="person col-sm-3 col-md-3">
-                    <!-- div renamed to personBelow because this one pic needs to be below all others so special css for it -->
-					<img src="images/nick.png" />
-					<p>Nicolas Stanzione</p>
-				</div>
-                </div>
-			</div>
             <?php require_once('footer.php');?> 
 		</div>
 	</body>
