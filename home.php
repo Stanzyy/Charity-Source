76d2e2d7a704e8b4133ec41fc2acf6ce653780ac
mlgGreggers <gregorysaras@gmail.com>
2015-04-29 18:09:12 -0400

    again
    ^^

--- home.php
+++ home.php
@@ -3,41 +3,47 @@
 <html lang='en'>
 	<head>
 		<meta charset='utf-8' />
-        <title>About | Charity Source</title>
-        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-        <?php require_once("css.php");?>
+		<title>About | Charity Source</title>
+
+		<!--Favicon links-->
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+		<!-- pulls in all our CSS -->
+		<?php require_once("css.php");?>
+
 	</head>
 	<body>
 		<div class="wrapper container">
+
+			<!-- brings in the header thats on all our pagess -->
 			<?php require_once('header.php');?>
-				<div class="imgWrapper row text-center">
-                    <img class="headingImg"src="images/workPicture.png" alt="Charity's Logo"/>
-                </div> <!--/.imgWrapper -->
-				<div class="aboutCenter" id="aboutCenter">
-					<div class="aboutItem">
-						<h2 class="title">About Us</h2>
-						<p>Charity Source will provide a fast, efficient way for users to donate to and support their 
-                        favorite charities throughout the United States. The main goal is to remove the ambiguity of what
-                        charities do with the donations they receive. It will also be an easy way to donate to a good cause
-                        that will leave people feeling comfortable knowing where their contributions are going.</p>
-					</div> <!--/. aboutItem -->
-					<div class="aboutItem">
-						<h2 class="title">What We're About</h2>
-						<p>Our team at Charity Source is dedicated to providing a safe and user friendly environment in which
-                        people can donate to good causes and feel great about it! We want to inspire users to search for good
-                        causes and give support where they know that every dollar they donate is making a difference.</p>
-					</div> <!--/. aboutItem -->
-					<div class="aboutItem">
-						<h2 class="title">Future Plans</h2>
-						<p>The future for Charity Source includes a lot of new features and partnerships with charities. Our team is in the process
-                        of developing a forum for our users to get in touch with other friendly folks or the charities themselves in order to join in 
-                        their events. Once we partner up with charities they will provide content to their pages to include future goals to show donators
-                        where their money is going.</p>
-					</div> <!--/. aboutItem -->
-				</div> <!--/. aboutCenter -->				
-			</div> <!--/.wrapper -->
-            <?php require_once('footer.php');?> 
+
+			<div class="imgWrapper row text-center">
+				<img class="headingImg"src="images/workPicture.png" alt="Charity's Logo"/>
+			</div>
+
+			<div class="aboutCenter" id="aboutCenter">
+				
+				<div class="aboutItem">
+					<h2 class="title">About Us</h2>
+					<p>Charity Source will provide a fast, efficient way for users to donate to and support their favorite charities throughout the United States. The main goal is to remove the ambiguity of what that will leave people feeling comfortable knowing where their contributions are going.</p>
+				</div>
+
+				<div class="aboutItem">
+					<h2 class="title">What We're About</h2>
+					<p>Our team at Charity Source is dedicated to providing a safe and user friendly environment in which people can donate to good causes and feel great about it! We want to inspire users to search for good causes and give support where they know that every dollar they donate is making a difference.</p>
+				</div>
+
+				<div class="aboutItem">
+					<h2 class="title">Future Plans</h2>
+					<p>The future for Charity Source includes a lot of new features and partnerships with charities. Our team is in the process of developing a forum for our users to get in touch with other friendly folks or the charities themselves in order to join in their events. Once we partner up with charities they will provide content to their pages to include future goals to show donors where their money is going.</p>
+				</div> <!--/. aboutItem -->
+			</div> <!--/. aboutCenter -->
+
+			<!-- pulls in our footer thats on every page -->
+			<?php require_once('footer.php');?> 
+		</div> <!--/.wrapper -->
 	</body>
 </html>
 <?php session_write_close();?>
