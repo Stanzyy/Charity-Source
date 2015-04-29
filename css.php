aaef34e50b635cba878f9db88137f61ce06b5721
mlgGreggers <gregorysaras@gmail.com>
2015-04-29 17:45:51 -0400

    Same as the last time
    Formatting

--- css.php
+++ css.php
@@ -1,3 +1,5 @@
-<!-- Latest compiled and minified CSS -->
+<!-- Latest compiled and minified bootstrap css-->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+<!--Our Custom CSS file-->
 <link rel="stylesheet" type="text/css" href="css/bootstrapCSS.css">
--- footer.php
+++ footer.php
@@ -1,7 +1,14 @@
 <footer>
     <p>&copy; <?php echo date('Y');?> Charity Source</p>
 </footer>
+
+<!--Script Includes-->
+
+<!--jQuery - latest minified version -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+<!--JavaScript to run the signout feature in the header-->
 <script src="javascripts/header.js"></script>
-<!-- Latest compiled and minified JavaScript -->
+
+<!-- Latest minified bootstrap javascript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
